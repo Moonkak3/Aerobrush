431ec70ca4d29348ee2401773557f5cb303e9ec3
Yuebin <yuebin0303@gmail.com>
2024-08-21 17:22:58 +0800

    added suport for multiple clients

--- frontend/src/components/DrawingCanvas.vue
+++ frontend/src/components/DrawingCanvas.vue
@@ -20,6 +20,7 @@ export default {
     data() {
         return {
             isDrawing: false,
+            receivedPaths: null,
             path: null,
             eraseLayer: null,
             lazyRadius: 10,
@@ -37,13 +38,33 @@ export default {
         this.$refs.background.height = 1080;
         this.$refs.background.left = (window.innerWidth - 1920) / 2;
         this.$refs.background.top = (window.innerHeight - 1080) / 2;
-
         paper.setup(this.$refs.canvas);
+
+        this.receivedPaths = {};
+
+        // Initialize WebSocket connection
+        this.websocket = new WebSocket("ws://localhost:8765");
+
+        this.websocket.onopen = () => {
+            console.log("WebSocket connection opened");
+        };
+
+        this.websocket.onmessage = (event) => {
+            const receivedData = JSON.parse(event.data);
+            this.handleReceivedData(receivedData);
+        };
+
+        this.websocket.onclose = () => {
+            console.log("WebSocket connection closed");
+        };
     },
     beforeUnmount() {
         paper.projects.forEach((project) => {
             project.remove();
         });
+        if (this.websocket) {
+            this.websocket.close();
+        }
     },
     methods: {
         startDrawing() {
@@ -64,7 +85,24 @@ export default {
                     blendMode: "destination-out",
                 });
             }
-
+            const pathData = {
+                points: this.path.segments.map((segment) => ({
+                    x: segment.point.x,
+                    y: segment.point.y,
+                })),
+                strokeColor: this.brush.color,
+                strokeWidth: this.path.strokeWidth,
+                opacity: this.path.opacity,
+                blendMode: this.path.blendMode,
+            };
+            if (
+                this.websocket &&
+                this.websocket.readyState === WebSocket.OPEN
+            ) {
+                this.websocket.send(
+                    JSON.stringify({ action: "startDrawing", path: pathData })
+                );
+            }
             this.isDrawing = true;
             paper.view.draw();
         },
@@ -78,10 +116,24 @@ export default {
 
             this.path.add(new paper.Point(bx, by));
             this.path.smooth({ type: "continuous" });
+            if (
+                this.websocket &&
+                this.websocket.readyState === WebSocket.OPEN
+            ) {
+                this.websocket.send(
+                    JSON.stringify({ action: "draw", point: { x: bx, y: by } })
+                );
+            }
         },
         stopDrawing() {
             if (!this.path) {
                 return;
+            }
+            if (
+                this.websocket &&
+                this.websocket.readyState === WebSocket.OPEN
+            ) {
+                this.websocket.send(JSON.stringify({ action: "stopDrawing" }));
             }
             this.isDrawing = false;
             this.path.simplify();
@@ -92,6 +144,56 @@ export default {
             raster.blendMode = blendMode;
 
             paper.view.draw();
+        },
+        handleReceivedData(data) {
+            // Reconstruct the path from received data
+            switch (data.action) {
+                case "startDrawing": {
+                    const receivedPath = new paper.Path({
+                        strokeColor: data.path.strokeColor,
+                        strokeWidth: data.path.strokeWidth,
+                        opacity: data.path.opacity,
+                        strokeCap: "round",
+                        blendMode: data.path.blendMode,
+                    });
+                    this.receivedPaths[data.id] = receivedPath;
+                    break;
+                }
+                case "draw": {
+                    this.receivedPaths[data.id].add(
+                        new paper.Point(data.point.x, data.point.y)
+                    );
+                    this.receivedPaths[data.id].smooth({ type: "continuous" });
+                    break;
+                }
+                case "stopDrawing": {
+                    this.receivedPaths[data.id].simplify();
+                    let raster = this.receivedPaths[data.id].rasterize();
+                    raster.blendMode = this.receivedPaths[data.id].blendMode;
+                    this.receivedPaths[data.id].remove();
+                    this.receivedPaths[data.id] = null;
+                    paper.view.draw();
+                    break;
+                }
+            }
+            // const receivedPath = new paper.Path({
+            //     strokeColor: data.strokeColor,
+            //     strokeWidth: data.strokeWidth,
+            //     opacity: data.opacity,
+            //     strokeCap: "round",
+            //     blendMode: data.blendMode,
+            // });
+            // data.points.forEach((point) => {
+            //     receivedPath.add(new paper.Point(point.x, point.y));
+            // });
+            // receivedPath.smooth({ type: "continuous" });
+
+            // receivedPath.simplify();
+            // const raster = receivedPath.rasterize();
+            // receivedPath.remove();
+            // raster.blendMode = data.blendMode;
+
+            // paper.view.draw();
         },
     },
 };
